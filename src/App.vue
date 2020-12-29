dd857433461df3e4eec0acbc8574f678ab5d6a6a
jiang-N <yeah_20@163.com>
2020-12-29 16:13:15 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Jiang/>
+</template>
+
+<script>
+import Jiang from './components/Jiang.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Jiang
+  }
+}
+</script>
