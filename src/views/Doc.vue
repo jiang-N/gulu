062d071ebcbee77942000dfa032e7d577dd41d6e
jiang-N <yeah_20@163.com>
2021-02-01 10:54:22 +0800

    调节 topNav 和 aside 的位置

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -31,6 +31,24 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 70px 16px 16px;
 
+  > h2 {
+    margin-bottom: 4px;
+  }
+
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+;
+}
 </style>
