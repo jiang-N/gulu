fac3395ebfe962a3faca76481b4ef4470b5ff5b8
jiang-N <yeah_20@163.com>
2021-02-05 14:06:04 +0800

    添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,6 +3,9 @@
   <h1>示例1</h1>
   <div>
     <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
 
@@ -11,7 +14,13 @@ import Button from '../lib/Button.vue';
 
 export default {
   name: 'SwitchDemo',
-  components: {Button}
+  components: {Button},
+  setup() {
+    const onClick = () => {
+      console.log('hi');
+    };
+    return {onClick};
+  }
 };
 </script>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,23 @@
 <template>
-  <button>
+  <button class="gulu-button" :class="`theme-${theme}`">
     <slot/>
   </button>
 </template>
 
-<script>
+<script lang="ts">
 export default {
-  name: 'Button'
-}
+  name: 'Button',
+  props: {
+    theme: {
+      type: String,
+      default: 'button'
+    }
+  }
+};
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
 </style>
