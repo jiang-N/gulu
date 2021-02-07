49a6e4d0de73ecf9be8fb5a0b060e7eaead58ca0
jiang-N <yeah_20@163.com>
2021-02-07 15:56:56 +0800

    openDialog 一句话打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -13,12 +13,15 @@
       <strong>加粗的标题</strong>
     </template>
   </Dialog>
+  <h1>示例2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
 import Button from '../lib/Button.vue';
 import {ref} from 'vue';
+import {openDialog} from '../lib/openDialog';
 
 export default {
   name: 'SwitchDemo',
@@ -31,9 +34,20 @@ export default {
     const f1 = () => {
       return false;
     };
-    const f2 = () => {
+    const f2 = () => {};
+    const showDialog = () => {
+      openDialog({
+        title: '标题',
+        content: '你好',
+        ok() {
+          console.log('ok');
+        },
+        cancel() {
+          console.log('cancel');
+        }
+      });
     };
-    return {x, toggle, f1, f2};
+    return {x, toggle, f1, f2, showDialog};
   }
 };
 </script>
