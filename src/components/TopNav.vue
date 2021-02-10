0a4d51bab9dc03cf1ba53e03e4e67bbd4022ca04
jiangnan <yeah_20@163.com>
2021-02-10 17:15:30 +0800

    从 iconfont.cn 引入 icon 图标

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="top-nav">
-    <div class="log">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-logo"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -25,7 +29,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$color: #007974;
+
 .top-nav {
+  color: $color;
   display: flex;
   padding: 16px;
   position: fixed;
@@ -39,6 +46,11 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+
+    > svg {
+      width: 32px;
+      height: 32px;
+    }
   }
 
   > .menu {
