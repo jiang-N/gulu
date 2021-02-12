d706abd1766e1911fbf7068a545b4f5944281f62
jiangnan <yeah_20@163.com>
2021-02-12 15:07:08 +0800

    封装 Markdown 优化代码

---
+++ src/components/Markdown.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="markdown-body" v-html="content"/>
+</template>
+
+<script lang="ts">
+import {ref} from 'vue';
+
+export default {
+  name: 'Markdown',
+  props: {
+    path: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const content = ref<String>(null);
+    import(props.path).then(result => {
+      content.value = result.default;
+    });
+    return {content};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
